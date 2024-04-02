18a754ef3136d870d8a6ba68a30f4b49e5190286
jenuc <jenuvchacko@gmail.com>
2024-04-02 11:23:46 -0500

    black formatter on qupathrunner

--- minimal_qupathrunner.py
+++ minimal_qupathrunner.py
@@ -5,59 +5,58 @@ from skimage import io, img_as_uint, transform
 import os
 import numpy as np
 import glob
-#import pathlib
 import shutil
 import sys
 import re
 
-# TODO change log entirely to json scheme within folde
-# TODO change debugmode printing to logging (not sure std pipe can handle log)
-debug_mode = 1 # 0 OFF 1 WARN 2 DEBUG 
+# import pathlib
 
-#print(sys.path)
+# TODO change log entirely to json scheme within folder
+# TODO change debug mode printing to logging (not sure std pipe can handle log)
+debug_mode = 1  # 0 OFF 1 WARN 2 DEBUG
+
+# print(sys.path)
 cwd = os.getcwd()
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+
 def read_TileConfiguration_coordinates(tile_config_path) -> list:
     coordinates = []
-    with open(tile_config_path, 'r') as file:
+    with open(tile_config_path, "r") as file:
         for line in file:
             # Extract coordinates using regular expression
-            match = re.search(r'\((-?\d+\.\d+), (-?\d+\.\d+)\)', line)
+            match = re.search(r"\((-?\d+\.\d+), (-?\d+\.\d+)\)", line)
             if match:
                 x, y = map(float, match.groups())
                 coordinates.append([x, y])
     return np.array(coordinates)
 
 
-def simulate_4_tiles(core, delta_xy = 500.0):
+def simulate_4_tiles(core, delta_xy=500.0):
     xy = core.get_xy_stage_position()
-    X = xy.x 
-    Y = xy.y 
+    X = xy.x
+    Y = xy.y
 
-    coordinates = np.array([[X,Y ],
-                    [X+delta_xy, Y],
-                    [X+delta_xy, Y+delta_xy], 
-                    [X, Y+delta_xy]])
-    #coordinates = np.array(coordinates)
+    coordinates = np.array(
+        [[X, Y], [X + delta_xy, Y], [X + delta_xy, Y + delta_xy], [X, Y + delta_xy]]
+    )
+    # coordinates = np.array(coordinates)
     return coordinates
 
 
-if len(sys.argv) == 5:  
-    if debug_mode>0:
-        print("QuPath: Arguments passed, using them", sys.argv)  
+if len(sys.argv) == 5:
+    if debug_mode > 0:
+        print("QuPath: Arguments passed, using them", sys.argv)
     self_filename, projectsFolderPath, sampleLabel, scan_type, region = sys.argv
 else:
-    if debug_mode>0:
-        print("QuPath: Incorrect number of arguments using default values")    
-    self_filename = r'C:\Users\lociuser\Codes\smart-wsi-scanner\minimal_qupathrunner.py'
-    projectsFolderPath =  r'C:\Users\lociuser\Codes\MikeN\data\slides'
-    sampleLabel = 'First_Test3'
-    scan_type = '4x_bf_1' 
-    region= 'bounds' # or a centeroid from the qupath annotation. eg "2012-2323"
-    #TODO may change to universal centroid_index naming
-
-
+    if debug_mode > 0:
+        print("QuPath: Incorrect number of arguments using default values")
+    self_filename = r"C:\Users\lociuser\Codes\smart-wsi-scanner\minimal_qupathrunner.py"
+    projectsFolderPath = r"C:\Users\lociuser\Codes\MikeN\data\slides"
+    sampleLabel = "First_Test3"
+    scan_type = "4x_bf_1"
+    region = "bounds"  # or a centroid from the qupath annotation. eg "2012-2323"
+    # TODO may change to universal centroid_index naming
 
 
 def init_pycromanager():
@@ -68,14 +67,14 @@ def init_pycromanager():
     return core, studio
 
 
-## Config Loading
+# Config Loading
 config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
 config = {**config["user_config"], **config["model_config"], **config["hard_config"]}
 lsm_pixel_size_base = config["pixel-size-shg-base"]
 bf_4x_pixel_size_base = config["pixel-size-bf-4x"]
 bf_20x_pixel_size_base = config["pixel-size-bf-20x"]
 camera_resolution_base = config["camera-resolution"]
-if debug_mode>1:
+if debug_mode > 1:
     print("QuPath: Loaded Config File")
 
 
@@ -87,25 +86,27 @@ brightfield_20x_background_fname = (
 )
 
 core, studio = init_pycromanager()
-if debug_mode>1:
+if debug_mode > 1:
     print("QuPath: Pycromanager Initialized")
 
 if core.is_sequence_running():
     studio.live().set_live_mode(False)
 
 qupath_project_folder = os.path.join(projectsFolderPath, sampleLabel)
-path_to_TileConfiguration = os.path.join(qupath_project_folder, scan_type, region, "TileConfiguration_transformed.txt") 
+path_to_TileConfiguration = os.path.join(
+    qupath_project_folder, scan_type, region, "TileConfiguration_transformed.txt"
+)
 if os.path.exists(path_to_TileConfiguration):
     coordinates = read_TileConfiguration_coordinates(path_to_TileConfiguration)
-    if debug_mode>0:
+    if debug_mode > 0:
         print("QuPath: Read TileConfiguration Coordinates")
 else:
-    if debug_mode>0:
+    if debug_mode > 0:
         print("QuPath: TileConfiguration file not found, simulating 4 tiles")
-    coordinates = simulate_4_tiles(core, delta_xy = 500.0)
+    coordinates = simulate_4_tiles(core, delta_xy=500.0)
 
-save_path = os.path.join(qupath_project_folder,scan_type) #"data/acquisition"
-#if not os.path.exists(save_path):
+save_path = os.path.join(qupath_project_folder, scan_type)  # "data/acquisition"
+# if not os.path.exists(save_path):
 #    os.makedir(save_path)
 
 sp_acq = SPAcquisition(
@@ -115,49 +116,49 @@ sp_acq = SPAcquisition(
     bf_4x_bg=io.imread(brightfield_4x_background_fname),
     bf_20x_bg=io.imread(brightfield_20x_background_fname),
 )
-if debug_mode>0:
+if debug_mode > 0:
     print("QuPath: Loaded settings to MM2")
 
-config['Z-stage-4x'] = 0.0
-config['hard-limit-x'] = [0,40000.0]
-config['hard-limit-y'] = [0,30000.0]
+config["Z-stage-4x"] = 0.0
+config["hard-limit-x"] = [0, 40000.0]
+config["hard-limit-y"] = [0, 30000.0]
 
 acq_name = sampleLabel + "-4x-bf"
 
 
-for x0,y0 in coordinates:
-    if  config['hard-limit-x'][0] < x0 < config['hard-limit-x'][1]:
+for x0, y0 in coordinates:
+    if config["hard-limit-x"][0] < x0 < config["hard-limit-x"][1]:
         coordinates_within_limits_x = True
     else:
-        print(f'{x0=} is out of range')
+        print(f"{x0=} is out of range")
         coordinates_within_limits_x = False
         break
 
-    if  config['hard-limit-y'][0] < y0 < config['hard-limit-y'][1]:
+    if config["hard-limit-y"][0] < y0 < config["hard-limit-y"][1]:
         coordinates_within_limits_y = True
     else:
-        print(f'{y0=} is out of range')
+        print(f"{y0=} is out of range")
         coordinates_within_limits_y = False
 
 coordinates_within_limits = coordinates_within_limits_y and coordinates_within_limits_x
 
-if debug_mode>0:
+if debug_mode > 0:
     print(f"QuPath: Coordinates within limits {coordinates_within_limits}")
 
-#fov_x = config["pixel-size-bf-4x"] * 1392  
-#fov_y = config["pixel-size-bf-4x"] * 1040  
-#coordinates[:,1] -= fov_y
-#coordinates[:,0] += fov_x/4
+# fov_x = config["pixel-size-bf-4x"] * 1392
+# fov_y = config["pixel-size-bf-4x"] * 1040
+# coordinates[:,1] -= fov_y
+# coordinates[:,0] += fov_x/4
 
-sp_acq.config[
-    "autofocus-speed"
-] = 4  # default is 4 ## `1-6`, the larger the faster, but potentially worse autofocusing resuls.
+sp_acq.config["autofocus-speed"] = (
+    4  # default is 4 ## `1-6`, the larger the faster, but potentially worse autofocus results.
+)
 
 
 core.set_auto_shutter(False)
 core.set_shutter_open(True)
 
-if debug_mode>0:
+if debug_mode > 0:
     print("QuPath: Starting Acquisition")
 if coordinates_within_limits:
     results_4x = sp_acq.whole_slide_bf_scan(
@@ -171,7 +172,7 @@ if coordinates_within_limits:
     core.set_auto_shutter(True)
     acq_id = len(glob.glob(os.path.join(save_path, acq_name + "*")))
     acq_path = os.path.join(save_path, acq_name + "_{}".format(acq_id))
-    if debug_mode>0:
+    if debug_mode > 0:
         print(f"QuPath: Saved files to {acq_path}")
 
     position_list = coordinates
@@ -181,33 +182,38 @@ if coordinates_within_limits:
 
     pixel_size = config["pixel-size-bf-4x"]
 
-    #mage_list = glob.glob(os.path.join(acq_path, "*.tif"))
-    image_list = sorted(glob.glob(os.path.join(acq_path, "*.tif")),key = lambda x: int(os.path.basename(x).split('-')[0]))
+    # mage_list = glob.glob(os.path.join(acq_path, "*.tif"))
+    image_list = sorted(
+        glob.glob(os.path.join(acq_path, "*.tif")),
+        key=lambda x: int(os.path.basename(x).split("-")[0]),
+    )
     stitchfolder_path = os.path.join(acq_path, "stitch")
     if not os.path.exists(stitchfolder_path):
         os.mkdir(stitchfolder_path)
 
-#    assert len(position_list) == len( image_list ), "Number of images does not match number of positions"
+    #    assert len(position_list) == len( image_list ), "Number of images does not match number of positions"
 
-    with open(os.path.join(stitchfolder_path, "TileConfiguration.txt"), "w") as text_file:
+    with open(
+        os.path.join(stitchfolder_path, "TileConfiguration.txt"), "w"
+    ) as text_file:
         print("dim = {}".format(2), file=text_file)
         for pos in range(position_list.shape[0]):
-            x = float(position_list[pos][0]) #int(position_list[pos][0] / pixel_size)
-            y = float(position_list[pos][1]) #int(position_list[pos][1] / pixel_size)
+            x = float(position_list[pos][0])  # int(position_list[pos][0] / pixel_size)
+            y = float(position_list[pos][1])  # int(position_list[pos][1] / pixel_size)
             print("{}.tif; ; ({}, {})".format(pos, x, y), file=text_file)
 
-    if debug_mode>0:
+    if debug_mode > 0:
         print(f"QuPath: Saved new TileConfiguration.txt to {stitchfolder_path}")
         print(f"QuPath: tif-files in acq folder :{len(image_list)}")
-        if debug_mode>1:
+        if debug_mode > 1:
             print(f"QuPath: Image List     :{image_list}   ")
-            #print(f"QuPath: Position List  :{position_list}")
+            # print(f"QuPath: Position List  :{position_list}")
 
     for pos in range(len(image_list)):
-        
+
         fn = image_list[pos]
         img = io.imread(fn)
-        
+
         correction = False
         rotate = False
         flip_y = True
@@ -226,30 +232,33 @@ if coordinates_within_limits:
         if flip_x:
             img = img[:, ::-1]
 
-        if debug_mode>1:
+        if debug_mode > 1:
             print(f"QuPath: Moving {pos}.tif to {stitchfolder_path}")
 
-        save_filename =os.path.join(stitchfolder_path , f"{pos}.tif")
+        save_filename = os.path.join(stitchfolder_path, f"{pos}.tif")
 
-        ##TODO: replace scikit iosave with tifffile with metadata
+        # TODO: replace scikit iosave with tifffile with metadata
         io.imsave(
             save_filename,
             img_as_uint(img),
             check_contrast=False,
         )
-        if debug_mode>1:
+        if debug_mode > 1:
             print(f"QuPath: Saved: {save_filename}")
 
-    qupath_stitching_folder = os.path.join(projectsFolderPath,sampleLabel,scan_type,region)
-    if debug_mode>0:
+    qupath_stitching_folder = os.path.join(
+        projectsFolderPath, sampleLabel, scan_type, region
+    )
+    if debug_mode > 0:
         print("QuPath: Stripping Metadata For stitching ")
-        print(f"QuPath: copying from \n{stitchfolder_path} \t to \n{qupath_stitching_folder}")
+        print(
+            f"QuPath: copying from \n{stitchfolder_path} \t to \n{qupath_stitching_folder}"
+        )
 
-    shutil.copytree(stitchfolder_path,
-                    qupath_stitching_folder,dirs_exist_ok=True)
+    shutil.copytree(stitchfolder_path, qupath_stitching_folder, dirs_exist_ok=True)
 
     shutil.rmtree(stitchfolder_path)
-    if debug_mode>0:
+    if debug_mode > 0:
         print(f"QuPath: Finished saving tiles for stitching at {stitchfolder_path}")
     os.chdir(cwd)
 
@@ -257,6 +266,5 @@ core._close()
 studio._close()
 del studio
 del core
-if debug_mode>0:
-    print("QuPath: Pycromanager Acqusition Task Completed")
-
+if debug_mode > 0:
+    print("QuPath: Pycromanager Acquisition Task Completed")
