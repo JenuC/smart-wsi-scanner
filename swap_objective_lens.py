24407f5ae8d72ab7b7adc81f2fdd2dcec510a43f
jenuc <jenuvchacko@gmail.com>
2024-04-15 15:01:19 -0500

    changed the lamp under condition instead of init location

--- swap_objective_lens.py
+++ swap_objective_lens.py
@@ -10,24 +10,22 @@ qp = smartpath_qpscope()
 
 if core.get_property(*camm.obj_slider) == camm_.CAMM_20X_BF.objective_position_label:
     camm.imaging = camm_.CAMM_20X_BF
-    core.set_property(*camm.lamp, 4)
 if core.get_property(*camm.obj_slider) == camm_.CAMM_4X_BF.objective_position_label:
     camm.imaging = camm_.CAMM_4X_BF
-    core.set_property(*camm.lamp, 2)
 ## TODO: implement CAMM_20x_MPM/BF update from Core Camera and move lamp to that section
 if core.get_property(*camm.obj_slider) == camm_.CAMM_20X_MPM.objective_position_label:
     camm.imaging = camm_.CAMM_20X_BF
-    core.set_property(*camm.lamp, 0)
 
 
 if len(sys.argv) == 2:
     if sys.argv[1].upper().startswith("20X"):
         sp.swap_objective_lens(core, camm, camm_.CAMM_20X_BF)
-
+        core.set_property(*camm.lamp, 4)
         print("QP: moved to 20X")
     elif sys.argv[1].upper().startswith("4X"):
         sp.swap_objective_lens(core, camm, camm_.CAMM_4X_BF)
         print("QP: moved to 4X")
+        core.set_property(*camm.lamp, 2)
     else:
         print(f"{sys.argv}", file=sys.stderr)
         sys.exit(1)
