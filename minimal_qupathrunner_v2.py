472aa3fd7f2ad34efb7d823518369efc7a87748f
jenuc <jenuvchacko@gmail.com>
2024-04-25 12:59:39 -0500

    updated min_runner2 with input arguments

--- minimal_qupathrunner_v2.py
+++ minimal_qupathrunner_v2.py
@@ -1,9 +1,20 @@
 from smartpath import *
 from smartpath_qpscope import *
+import sys
 
 sp = smartpath(core)
 qp = smartpath_qpscope()
 q = qpscope_project()
+
+if len(sys.argv) == 5:
+    self_filename, projectsFolderPath, sampleLabel, scan_type, region = sys.argv
+else:
+    self_filename = r"C:\Users\lociuser\Codes\smart-wsi-scanner\minimal_qupathrunner.py"
+    projectsFolderPath = r"C:\Users\lociuser\Codes\MikeN\data\slides"
+    sampleLabel = "First_Test3"
+    scan_type = "4x_bf_1"
+    region = "bounds"  # or a centroid from the qupath annotation. eg "2012-2323"
+    # TODO may change to universal centroid_index naming
 
 if core.get_property(*camm.obj_slider) == camm_.CAMM_20X_BF.objective_position_label:
     camm.imaging = camm_.CAMM_20X_BF
@@ -17,11 +28,11 @@ q = qpscope_project()
 q.path_tile_configuration
 
 q = qpscope_project(
-    projectsFolderPath=r"C:\Users\lociuser\Codes\MikeN\data\slides",
-    sampleLabel="First_Test",
-    scan_type="4x_bf_1",
-    region="2447_1631",
-    tile_config="TileConfiguration.txt",
+    projectsFolderPath=projectsFolderPath,
+    sampleLabel=sampleLabel,
+    scan_type=scan_type,
+    region=region,
+    tile_config="TileConfiguration_QP.txt",
 )
 positions = qp.read_TileConfiguration_coordinates(q.path_tile_configuration)
 af_position_indices = qp.get_autofocus_positions(positions, camm, 3)
@@ -32,5 +43,5 @@ qp.scan_using_positions(
     positions=positions,
     id1=q.acq_id,
     core=core,
-    autofocus_indices=autofocus_indices,
+    autofocus_indices=af_position_indices,
 )
