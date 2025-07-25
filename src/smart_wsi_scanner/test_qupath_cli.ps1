4028386f94f48be024e664a3f98398bdf43a61df
JenuC <jenu.chacko@wisc.edu>
2025-07-24 22:39:55 -0500

    testing frm CLI

--- src/smart_wsi_scanner/test_qupath_cli.ps1
+++ src/smart_wsi_scanner/test_qupath_cli.ps1
@@ -1,7 +1,24 @@
 # Define the strings
-$string1 = "value1"
-$string2 = "value2"
-$string3 = "value3"
+$x = "-1260"
+$y = "5000"
+$z = "-300"
+$r = "85"
 
-# Call the Python script with the strings as arguments
-python acq.py $string1 $string2 $string3
+#XY
+    #get_stageXY
+    #move_stageXY -x $x -y $y
+    #get_stageXY
+#Z
+    #get_stageZ
+    #move_stageZ -z $z
+    #get_stageZ
+
+#R
+    #get_stageR
+    #move_stageR -angle $r
+    #get_stageR
+
+#
+acquisition_workflow 
+
+#snap 
